6e2bcadde19f10677a1f5592b0e9d6fcf00cbb7b
Spencer Radtke <spencert.t.radtke@gmail.com>
2026-02-28 16:27:41 +0100

    removed public folder; still trying to get local dev and github working on the same project structure.

--- home.js
+++ home.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + '/node_modules'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/pages'));
+app.use(express.static(__dirname + '/models'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

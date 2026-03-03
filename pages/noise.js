4fdd39a8609d234c6f21be5faa858be9453eac6e
Spencer Radtke <spencert.t.radtke@gmail.com>
2026-03-03 12:08:25 +0100

    added stats.

--- pages/noise.js
+++ pages/noise.js
@@ -1,16 +1,19 @@
 import * as THREE from 'three';
+import Stats from 'three/addons/libs/stats.module.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import * as GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.21';
 import * as TILEMAP from '../src/tilemap.js'
 
 const scene = new THREE.Scene();
+const stats = new Stats();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 8000);
 camera.up.set(0, 0, 1);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+document.body.appendChild(stats.dom);
 
 scene.background = new THREE.Color(0x2f8fff);
 scene.add(new THREE.AmbientLight(0xffffff, 0.1));
@@ -87,6 +90,7 @@ function animate(time) {
 
 	// controls.target.set(TILEMAP.mapCfg.chunkSize * TILEMAP.mapCfg.tileSize / 2, TILEMAP.mapCfg.chunkSize * TILEMAP.mapCfg.tileSize / 2, 0);
 	// controls.update();
+	stats.update();
 	renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
